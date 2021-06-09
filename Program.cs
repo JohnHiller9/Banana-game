26c30ec5433a83d1f37614e0d761b8ef1813f318
John Hiller <jdhiller@bellsouth.net>
2021-06-09 11:11:10 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your favorite yellow-colored, boomerang-shaped fruit with a peel?!");
+            var fruit = Console.ReadLine();
+            Console.WriteLine($"Nana, nana, bo-bana, banana-fana fo-fana, fee-fi-mo-mana, {fruit}!");
+        }
+    }
+}
